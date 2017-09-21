d2c502167af570605d38d5163eea38158b7e26a9
AshleyCheny <AshleyCheny@gmail.com>
2017-09-20 21:42:13 -0700

    Changed the data object to an array with multiple objects and assign the new data to the controller Used directive ng-repeat to loop through the products array

--- js/app.js
+++ js/app.js
@@ -2,18 +2,17 @@
 (function(){
 
   // data object
-  var gem = {
-    name: 'Azurite',
-    price: 2.95,
-    canPurchase: false,
-    soldOut: false
-  };
+  var gems = [
+    { name: 'Azurite', price: 2.95 },
+    { name: 'Bloodstone', price: 5.95 },
+    { name: 'Zircon', price: 3.95 }
+  ];
 
   // create a module(app)
   var app = angular.module("gemStore", [ ]);
 
   // create a controller for the app and assign the property to the controller
   app.controller('StoreController', function(){
-    this.product = gem;
+    this.products = gems;
   });
 })();

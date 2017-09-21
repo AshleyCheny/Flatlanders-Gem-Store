514bd21ce7e025d09318b2eef6457827f6e9a55a
AshleyCheny <AshleyCheny@gmail.com>
2017-09-20 20:44:40 -0700

    Created an angular app and its controller Used directives ng-app, ng-controller, ng-hide, ng-show Used expressions to display data Updated readme file

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-var app = angular.module("gemStore", [ ]);
+// it's a good habit to put the script in a closure
+(function(){
+
+  // data object
+  var gem = {
+    name: 'Azurite',
+    price: 2.95,
+    canPurchase: false,
+    soldOut: false
+  };
+
+  // create a module(app)
+  var app = angular.module("gemStore", [ ]);
+
+  // create a controller for the app and assign the property to the controller
+  app.controller('StoreController', function(){
+    this.product = gem;
+  });
+})();
